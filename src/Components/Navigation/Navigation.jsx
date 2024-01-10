ce8966ebae7a7cd80bbaa5fe3a9fa694bdd01270
krutikmaru <krutikmaru18@gmail.com>
2024-01-10 23:37:40 +0530

    User profile Image update ✅

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -19,7 +19,7 @@ const Navigation = () => {
         <Link to="/account">
           <div className="w-9 h-9 rounded-full  bg-green-primary cursor-pointer relative overflow-hidden">
             <img
-              src="https://carzilla.m-fractal.com/files/meeraaa.jpg"
+              src="https://i.ytimg.com/vi/7udSzXYWOd0/hq720.jpg?sqp=-oaymwEYCJUDENAFSFryq4qpAwoIARUAAIhC0AEB&rs=AOn4CLCOC6fH67x3HSQJvqVJMhwrSXURhQ"
               className="absolute object-cover top-0 left-0 w-full h-full"
               alt="MEERA"
             />
